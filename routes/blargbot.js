28b316c816d32e390d5e0c1823ad3237ef201da1
RagingLink <brianbert03@gmail.com>
2020-07-20 15:59:41 +0200

    Update blargbot.js

--- routes/blargbot.js
+++ routes/blargbot.js
@@ -35,7 +35,7 @@ router.get("/tags", async (req, res, next) => {
   if(!req.query.xpath) req.query.xpath = '//';
 
   try {
-    const browser = await puppeteer.launch({headless: false, args : ["--no-sandbox"]});
+    const browser = await puppeteer.launch({executablePath: 'chromium-browser',headless: false, args : ["--no-sandbox"]});
     const [page] = await browser.pages();
 
     await page.goto('https://blargbot.xyz/tags');
