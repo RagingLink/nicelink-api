49a4b432a169c7c5ecec96e8ab7df1d6064fc035
Brian <nicelink.brian@gmail.com>
2021-04-06 10:54:27 +0200

    Use yarn instead and eco

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps : [{
+      script: './index.js',
+      watch: false,
+      name: 'api',
+      args: [
+        "--color",
+        "--raw"
+      ]
+    }],
+
+  };
