3ae9bb2e84eece8d28910a8dced6818093c046c7
RagingLink <nicelink.brian@gmail.com>
2021-04-06 11:32:01 +0200

    Math endpoint

--- routes/blargbot.js
+++ routes/blargbot.js
@@ -7,4 +7,5 @@ router.get("/test", (req, res) => {
 router.use("/shards", require("./blargbot/shards"));
 router.use("/tags", require("./blargbot/tags"));
 router.use("/stats", require("./blargbot/stats"));
+router.use('/math', require('./blargbot/math'));
 module.exports = router;
---
+++ routes/blargbot/math.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const {evaluate} = require('mathjs')
+router.post('/', (req, res, next) => {
+    var scope = {},
+        expression;
+    if(!req.body || !req.body.expression) return res.send({error: 'Invalid request', message: 'No provided body, you want to evaluate air?'});
+    expression = req.body.expression;
+    delete req.body.expression;
+    scope = req.body.scope || {}
+    delete req.body.scope;
+    scope = Object.assign(scope, req.body);
+
+    return res.send(JSON.stringify({output: evaluate(expression, scope), scope}));
+})
