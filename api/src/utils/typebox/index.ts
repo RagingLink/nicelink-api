7f4f2bfc0d66a6b110070c827bf776d11e5dc2bb
RagingLink <nicelink.brian@gmail.com>
2024-09-24 13:32:54 +0200

    Fix bug related to different this context

--- api/src/utils/typebox/index.ts
+++ api/src/utils/typebox/index.ts
@@ -2,13 +2,13 @@ import { Static, Type } from '@sinclair/typebox';
 import { TypeCompiler } from '@sinclair/typebox/compiler';
 
 export const GenericRecord = Type.Record(Type.String(), Type.Unknown());
-export const GenericRecordCheck = TypeCompiler.Compile(GenericRecord).Check;
 export type GenericRecordType = Static<typeof GenericRecord>;
+export const GenericRecordTypeCheck = TypeCompiler.Compile(GenericRecord);
 
 export const GenericObject = Type.Object({}, { additionalProperties: true });
-export const GenericObjectCheck = TypeCompiler.Compile(GenericObject).Check;
 export type GenericObjectType = Static<typeof GenericObject>;
+export const GenericObjectTypeCheck = TypeCompiler.Compile(GenericObject);
 
 export const GenericArray = Type.Array(Type.Unknown());
-export const GenericArrayCheck = TypeCompiler.Compile(GenericArray);
 export type GenericArrayType = Static<typeof GenericArray>;
+export const GenericArrayTypeCheck = TypeCompiler.Compile(GenericArray);
